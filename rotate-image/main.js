8a880d8705ef0c7b311f80d61e1fb6d5fc6c87a1
Siddhartha <sidduinevitable@gmail.com>
2020-02-28 21:31:50 +0530

    js solution for the rotate image

---
+++ rotate-image/main.js
@@ -0,0 +1,16 @@
+// https://app.codesignal.com/interview-practice/task/5A8jwLGcEpTPyyjTB/solutions
+
+function rotateImage(a) {
+  for (let i = 0; i < a.length; i++) {
+    for (let j = i; j < a.length; j++) {
+      [a[i][j], a[j][i]] = [a[j][i], a[i][j]];
+    }
+  }
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < (a.length / 2 | 0); j++) {
+      [a[i][j], a[i][a.length - j - 1]] = [a[i][a.length - j - 1], a[i][j]];
+    }
+  }
+  return a;
+
+}
